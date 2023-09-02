9ab86becaa41a66ffe81754fb901238a5deebe75
Andres Torres <andres.torressandoval@hotmail.com>
2023-09-02 11:32:45 -0500

    Slider de testimonios y modificaciones de cards

--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,9 +1,12 @@
+import { GET_USER_ALL, GET_USER_ID, GET_USER_NAME, FAILURE } from "./Users/Users"
+import { GET_SONG_ALL, GET_SONG_ALL_QUERY, GET_SONG_ALL_QUERY_FILTER,GET_SONG_ID, GET_SONG_NAME,GET_SONG_NAME_FILTER } from "./Songs/Songs"
+
 const initialState={
     generalUsers:[],
     copyusers:[],
     generalSongs:[],
     copySongs:[],
-    detailSongs:[],
+    detailSongs:[], 
     generalPlaylists:[],
     copyPlaylists:[],
 }

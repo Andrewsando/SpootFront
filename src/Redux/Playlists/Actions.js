77808026aa343f24770e56a189733656b17ce60e
Wpxchi <urielcastillo891@gmail.com>
2023-09-02 10:06:30 -0300

    reducer playlist y actions playlist

--- src/Redux/Playlists/Actions.js
+++ src/Redux/Playlists/Actions.js
@@ -1,44 +1,59 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4322/playlists"; 
+export const GET_PLAYLISTS='GET_PLAYLISTS'
+export const GET_PLAYLIST_ID='GET_PLAYLIST_ID'
+export const GET_PLAYLIST_NAME='GET_PLAYLIST_NAME'
+export const FAILURE='FAILURE'
 
-const makeRequest = async (dispatch, requestAction, successAction, failureAction, requestFn) => {
-  dispatch({ type: requestAction });
-
-  try {
-    const response = await requestFn();
-    dispatch({ type: successAction, payload: response.data });
-  } catch (error) {
-    dispatch({ type: failureAction, payload: error.message });
+export const getPlaylists=()=>{
+  return async function(dispatch){
+    try {
+      const res= await axios.get('http://localhost:3001/playlists')
+      dispatch({type: GET_PLAYLISTS, payload: res.data});
+    } catch (error) {
+      dispatch({ type: FAILURE, payload: error.message });
+    }
   }
-};
+}
 
-export const fetchPlaylists = (page, perPage) => async (dispatch) => {
-  const requestFn = () => axios.get(`${BASE_URL}?page=${page}&perPage=${perPage}`);
-  makeRequest(dispatch, FETCH_PLAYLISTS_REQUEST, FETCH_PLAYLISTS_SUCCESS, FETCH_PLAYLISTS_FAILURE, requestFn);
-};
+export const getPlaylistID=(id)=>{
+  return async function (dispatch){
+    try {
+      const res = await axios.get(`http://localhost:3001/playlists/${id}`);
+      dispatch({type: GET_PLAYLIST_ID, payload: res.data});
+    } catch (error) {
+      dispatch({ type: FAILURE, payload: error.message });
+    }
+  }
+}
 
-export const fetchPlaylistById = (id) => async (dispatch) => {
-  const requestFn = () => axios.get(`${BASE_URL}/${id}`);
-  makeRequest(dispatch, FETCH_PLAYLIST_BY_ID_REQUEST, FETCH_PLAYLIST_BY_ID_SUCCESS, FETCH_PLAYLIST_BY_ID_FAILURE, requestFn);
-};
+export const getPlaylistName=(name)=>{
+  return async function (dispatch){
+    try {
+      const res= await axios.get(`http://localhost:3001/playlists?name=${name}`);
+      dispatch({type:GET_PLAYLIST_NAME, payload: res.data});
+    } catch (error) {
+      dispatch({ type: FAILURE, payload: error.message });
+    }
+  }
+}
 
-export const fetchPlaylistsByName = (name, page, perPage) => async (dispatch) => {
-  const requestFn = () => axios.get(`${BASE_URL}?name=${name}&page=${page}&perPage=${perPage}`);
-  makeRequest(dispatch, FETCH_PLAYLISTS_BY_NAME_REQUEST, FETCH_PLAYLISTS_BY_NAME_SUCCESS, FETCH_PLAYLISTS_BY_NAME_FAILURE, requestFn);
-};
+export const putPlayist=(id, dataPlaylist)=>{
+  return async function (dispatch){
+    try {
+      await axios.put(`http://localhost:3001/playlists/${id}`, dataPlaylist);
+    } catch (error) {
+      dispatch({ type: FAILURE, payload: error.message });
+    }
+  }
+}
 
-export const addPlaylist = (name, description) => async (dispatch) => {
-  const requestFn = () => axios.post(BASE_URL, { name, description });
-  makeRequest(dispatch, ADD_PLAYLIST_REQUEST, ADD_PLAYLIST_SUCCESS, ADD_PLAYLIST_FAILURE, requestFn);
-};
-
-export const updatePlaylist = (id, name, description) => async (dispatch) => {
-  const requestFn = () => axios.put(`${BASE_URL}/${id}`, { name, description });
-  makeRequest(dispatch, UPDATE_PLAYLIST_REQUEST, UPDATE_PLAYLIST_SUCCESS, UPDATE_PLAYLIST_FAILURE, requestFn);
-};
-
-export const deletePlaylist = (id) => async (dispatch) => {
-  const requestFn = () => axios.delete(`${BASE_URL}/${id}`);
-  makeRequest(dispatch, DELETE_PLAYLIST_REQUEST, DELETE_PLAYLIST_SUCCESS, DELETE_PLAYLIST_FAILURE, requestFn);
-};
+export const deletePlaylist=(id)=>{
+  return async function (dispatch){
+    try {
+      await axios.delete(`http://localhost:3001/playlists/${id}`)
+    } catch (error) {
+      dispatch({ type: FAILURE, payload: error.message });
+    }
+  }
+}
--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,6 +1,6 @@
 import { GET_USER_ALL, GET_USER_ID, GET_USER_NAME, FAILURE } from "./Users/Users"
 import { GET_SONG_ALL, GET_SONG_ALL_QUERY, GET_SONG_ALL_QUERY_FILTER,GET_SONG_ID, GET_SONG_NAME,GET_SONG_NAME_FILTER,FAILURE } from "./Songs/Songs"
-
+import { GET_PLAYLISTS, GET_PLAYLIST_ID, GET_PLAYLIST_NAME, FAILURE} from "./Playlists/Actions"
 const initialState={
     generalUsers:[],
     generalSongs:[],
@@ -13,6 +13,8 @@ const initialState={
 
 const rootReducer=(state= initialState, action)=>{
     switch(action.payload){
+
+      // Reducer para USERS
         case GET_USER_ALL: return {...state, generalUsers: action.payload, }
 
         case GET_USER_ID: return {...state, generalUsers: action.payload, }
@@ -20,6 +22,8 @@ const rootReducer=(state= initialState, action)=>{
         case GET_USER_NAME: return {...state, generalUsers: action.payload, }
 
         case FAILURE: return {...state, failure: action.payload}
+
+        //Reducer para SONGS
         
         case GET_SONG_ALL: return {...state, generalSongs: action.payload ,copySongs: action.payload}
 
@@ -32,6 +36,14 @@ const rootReducer=(state= initialState, action)=>{
         case GET_SONG_ALL_QUERY_FILTER: return {...state, detailSongs: action.payload}
 
         case GET_SONG_NAME_FILTER: return {...state, detailSongs: action.payload}
+
+        
+          // Reducer para PLAYLISTS
+        case GET_PLAYLISTS: return {...state, generalPlaylists: action.payload, copyPlaylists: action.payload}
+
+        case GET_PLAYLIST_ID: return {...state, generalPlaylists: action.payload, copyPlaylists: action.payload}
+
+        case GET_PLAYLIST_NAME: return {...state, generalPlaylists: action.payload, copyPlaylists: action.payload}
 
         default: return {...state}
     }

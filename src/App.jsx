24ec96fc2f9d864bf4b5dcc7493a1220054b5337
Imanol <imano.arias@gmail.com>
2023-09-03 16:29:47 -0300

    fix con el merge en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import React from "react";
 import Home from "../src/Pages/LandingPage/Home";
 import Team from "../src/Pages/AboutPage/Team";
 import WhatSpootChat from "../src/Pages/AboutPage/WhatSpootChat";
-import PrivacyPolicy from "../src/Pages/MeetPage/PrivacyPolicy";
-import TermsAndConditions from "../src/Pages/MeetPage/TermsAndConditions ";
 import Contact from "../src/Pages/ContactPage/Contact";
 import AccessTo from "./Pages/AccessPage/AccessForm";
+import UserProfile from "./Pages/UserPage/UserProfile";
+import PoliciesAndTerms from "./Pages/MeetPage/PoliciesAndTerms";
+import Support from "./Pages/SupportPage/Support";
 import Account from './Pages/AccountPage/Account.jsx';
 
 
@@ -15,21 +16,16 @@ export default function App() {
   return (
     // Enrutador principal
     <Routes>
-      {/* Ruta a la página principal, Landing Page */}
+      {/* Ruta a la página principal*/}
       <Route path="/" element={<Home />} />
       <Route path="/what-is-spootchat" element={<WhatSpootChat />} />
       <Route path="/meet-our-team" element={<Team />} />
-      <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-      <Route path="/privacy-policies" element={<PrivacyPolicy />} />
+      <Route path="/privacy-policy-and-terms-of-use" element={<PoliciesAndTerms />} />
       <Route path="/contact-us" element={<Contact />} />
       <Route path="/access-to" element={<AccessTo />} />
+      <Route path="/user" element={<UserProfile />} />
+      <Route path="/support" element={<Support />} />
       <Route path="/manage-my-account" element={<Account />} />
-
-      {/* <Route path="/user" element={<PonerPaginaCorrespondiente />} />
-    <Route path="/manage-my-account" element={<PonerPaginaCorrespondiente />} />
-    <Route path="/support" element={<PonerPaginaCorrespondiente />} />
-
-    */}
     </Routes>
   );
 }

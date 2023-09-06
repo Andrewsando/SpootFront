d843a439dacd0946d4e21521d5bb137e1f0be5cc
Valentina Ortiz <svalentinaog10@gmail.com>
2023-09-05 22:14:26 -0500

    Interfaz de login/register terminada (css), pendiente inplementar lógica

--- src/Pages/AccessPage/AccessForm.jsx
+++ src/Pages/AccessPage/AccessForm.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Validation from "../../Utils/Validation.jsx";
 import { loginUser } from "../../Redux/Actions/Users";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Footer from "./components/Footer";
 
 export default function AccessForm() {
   const dispatch = useDispatch();
@@ -45,41 +47,83 @@ export default function AccessForm() {
   };
 
   return (
-    <div className="container-formLogin">
-      <div className="content-log">
-        <div className="logo-form-container">
-          <img
-            src="/images/sonido.png"
-            alt="img-form"
-            className="logo-form"
-            name="image"
-          />
-        </div>
-        <h1 className="titleForm">Regístrate o Inicia Sesión</h1>
-      </div>
-      <div className="pageForm">
-        <form onSubmit={handleSubmit} className="form-create">
-          <div className="form-columns">
-            <div className="form-column">
-              <label htmlFor="email" className="form-create_label" />
-              <input
-                className={
-                  errors.name ? "form-create_inputError" : "form-create_input"
-                }
-                id="email-login"
-                placeholder="Introduce tu email o nombre de usuario"
-                type="email"
-                name="email"
-                value={userData.email}
-                onChange={handleChange}
-              />
-              <span className="spanError">{errors.email}</span>
-            </div>
+    <div className="container-general-formLogin">
+      <div className="container-formLogin">
+        <div className="content-log">
+          <div className="logo-form-container">
+            <img
+              src="/images/sonido.png"
+              alt="img-form"
+              className="logo-form"
+              name="image"
+            />
           </div>
-
-          <button className="form-button">Continuar</button>
-        </form>
+          <h1 className="titleForm">Regístrate o Inicia Sesión</h1>
+        </div>
+        <div className="pageForm">
+          <form onSubmit={handleSubmit} className="form-create">
+            <div className="form-columns">
+              <div className="form-column">
+                <label htmlFor="email" className="form-create_label" />
+                <input
+                  className={
+                    errors.name ? "form-create_inputError" : "form-create_input"
+                  }
+                  id="email-login"
+                  placeholder="Introduce tu email o nombre de usuario"
+                  type="email"
+                  name="email"
+                  value={userData.email}
+                  onChange={handleChange}
+                />
+                <span className="spanError">{errors.email}</span>
+              </div>
+            </div>
+            {/* Password */}
+            <div className="form-columns">
+              <div className="form-column">
+                <label htmlFor="password" className="form-create_label" />
+                <input
+                  className={
+                    errors.name ? "form-create_inputError" : "form-create_input"
+                  }
+                  id="password-login"
+                  placeholder="Introduce tu contraseña"
+                  type="password"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleChange}
+                />
+                <span className="spanError">{errors.password}</span>
+              </div>
+            </div>
+            <Link to="" className="forgotten-password">
+              <span>¿Olvidaste tu contraseña?</span>
+            </Link>
+            <button className="form-button">Continuar</button>
+            <h1 className="text-opcion">ó</h1>
+            <button className="form-continue-button">
+              <img
+                src="/images/spotify-white.png"
+                alt="icon"
+                name="image"
+                className="iconLog"
+              />
+              <span> Continúa con SpootChat</span>
+            </button>
+            <button className="form-continue-button">
+              <img
+                src="/images/google.png"
+                alt="icon"
+                name="image"
+                className="iconLog"
+              />
+              <span> Continúa con Google</span>
+            </button>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }

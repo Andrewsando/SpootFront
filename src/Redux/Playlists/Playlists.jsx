cfffc98c4ad5d625af880003a175018adcc9f918
Hawkins5 <hmasd05@gmail.com>
2023-09-13 18:47:30 -0500

    nuevo

---
+++ src/Redux/Playlists/Playlists.jsx
@@ -0,0 +1,24 @@
+<div className="bg-black text-white min-h-screen p-8">
+  <header className="text-4xl font-bold mb-8">Tus Listas de Reproducción</header>
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    
+    <div className="bg-gray-800 p-6 rounded-lg">
+      <h2 className="text-2xl font-semibold mb-2">Lista de Reproducción 1</h2>
+      <p className="text-gray-400">Descripción de la lista de reproducción.</p>
+    </div>
+
+   
+    <div className="bg-gray-800 p-6 rounded-lg">
+      <h2 className="text-2xl font-semibold mb-2">Lista de Reproducción 2</h2>
+      <p className="text-gray-400">Descripción de la lista de reproducción.</p>
+    </div>
+
+   
+    <div className="bg-gray-800 p-6 rounded-lg">
+      <h2 className="text-2xl font-semibold mb-2">Lista de Reproducción 3</h2>
+      <p className="text-gray-400">Descripción de la lista de reproducción.</p>
+    </div>
+
+    
+  </div>
+</div>

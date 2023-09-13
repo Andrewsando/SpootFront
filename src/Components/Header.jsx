edc7e606a3afa80c090d1025e34f33fbd38233df
Hawkins5 <hmasd05@gmail.com>
2023-09-13 17:09:24 -0500

    act

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,9 @@
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import { firebase } from "../../config/config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(firebase);
 
 export default function Header() {
   const [scrolling, setScrolling] = useState(false);

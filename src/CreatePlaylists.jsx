495f03473346a8cdf0e5e14df9410e5f2dc12692
Hawkins5 <hmasd05@gmail.com>
2023-09-15 18:07:11 -0500

    .

--- src/CreatePlaylists.jsx
+++ src/CreatePlaylists.jsx
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import Validate from "./validations";
+import { useState, useEffect} from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getSongName } from "../../../Redux/Actions/Songs";
+import { createPlaylist } from "../../../Redux/Actions/Playlists";
 
 const CreatePlaylist = () => {
+  const SongName = useSelector((state) => state.generalSongs);
+  const NewSongName = SongName.result;
+  const [errors, setErrors] = useState({});
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
   const [playlist, setPlaylist] = useState({
     image: "",
     name: "",
@@ -8,116 +19,129 @@ const CreatePlaylist = () => {
     description: "",
   });
 
+  useEffect(() => {
+    const validationErrors = Validate(playlist);
+    setErrors(validationErrors);
+  }, [playlist]);
+
   const HandleInputChange = (event) => {
-    const { name, value } = event.target;
-    setPlaylist({
+    const value = event.target.value;
+    const updatedForm = {
       ...playlist,
-      [name]: value,
-    });
+      [event.target.name]: value,
+    };
+    setPlaylist(updatedForm);
   };
 
-  const handleSelect = (e) => {
-    const selectedSong = e.target.value;
-    if (!playlist.songs.includes(selectedSong)) {
-      setPlaylist({
-        ...playlist,
-        songs: [...playlist.songs, selectedSong],
-      });
-    }
+  const handleSelect = (newSongName) => {
+    const updatedForm = {
+      ...playlist,
+      songs: [...playlist.songs, newSongName],
+    };
+    setPlaylist(updatedForm);
   };
 
-  const handleRemoveSong = (song) => {
-    const updatedSongs = playlist.songs.filter((s) => s !== song);
-    setPlaylist({
+  const HandleSearch = () => {
+    dispatch(getSongName(search));
+  };
+  const handleDeleteSearch=()=>{
+    dispatch(getSongName())
+    setSearch('')
+  }
+
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(createPlaylist(playlist));
+  };
+
+  const handleDeleteSong = (index) => {
+    const updatedSongs = [...playlist.songs];
+    updatedSongs.splice(index, 1); // Elimina la canción en el índice especificado
+    const updatedForm = {
       ...playlist,
       songs: updatedSongs,
-    });
-  };
-
-  const handleCreatePlaylist = async (e) => {
-    e.preventDefault();
-
-    if (!playlist.name || playlist.songs.length === 0) {
-      alert("Completa todos los campos requeridos.");
-      return;
-    }
-
-    const newPlaylist = {
-      name: playlist.name,
-      image: playlist.image,
-      description: playlist.description,
-      songs: playlist.songs,
     };
-
-    try {
-      const response = await fetch("URL_DE_TU_BACKEND/crear-lista-de-reproduccion", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Agrega otros encabezados según sea necesario, como el token de autenticación
-        },
-        body: JSON.stringify(newPlaylist),
-      });
-
-      if (response.ok) {
-        alert("¡Lista de reproducción creada exitosamente!");
-        // Redirige al usuario o realiza cualquier otra acción necesaria después de crear la lista
-      } else {
-        alert("No se pudo crear la lista de reproducción. Inténtalo de nuevo más tarde.");
-      }
-    } catch (error) {
-      console.error("Error al crear la lista de reproducción:", error);
-    }
+    setPlaylist(updatedForm);
   };
+ 
 
   return (
     <div>
-      <form>
-        <label>
-          Image:
-          <input type="text" name="image" value={playlist.image} onChange={HandleInputChange} />
+      <form onSubmit={HandleSubmit}>
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Imagen:
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="image"
+            value={playlist.image}
+            onChange={HandleInputChange}
+          />
+          {errors.image? <p>{errors.image}</p>:<p></p>}
+        </label>
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Nombre:
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="name"
+            value={playlist.name}
+            onChange={HandleInputChange}
+          />
+           {errors.name? <p>{errors.name}</p>:<p></p>}
         </label>
 
-        <label>
-          Name:
-          <input type="text" name="name" value={playlist.name} onChange={HandleInputChange} />
-        </label>
-
-        <label>
-          Songs:
-          <select name="songs" value="" onChange={handleSelect}>
-            <option value="" disabled>
-              Selecciona una canción
-            </option>
-            {/* Renderiza tus opciones de canciones aquí */}
-            {/* Ejemplo de opción de canción */}
-            <option value="nombre_de_la_cancion">Nombre de la canción</option>
-          </select>
-          <ul>
-            {/* Renderiza la lista de canciones seleccionadas */}
-            {playlist.songs.map((song) => (
-              <li key={song}>
-                {song}
-                <button type="button" onClick={() => handleRemoveSong(song)}>
-                  Eliminar
-                </button>
-              </li>
-            ))}
-          </ul>
-        </label>
-
-        <label>
+        <label className="block text-gray-700 text-sm font-bold mb-2">
           Description:
           <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             name="description"
             value={playlist.description}
             onChange={HandleInputChange}
           />
+          {errors.description? <p>{errors.description}</p>:<p></p>}
         </label>
-
-        <button onClick={handleCreatePlaylist}>Crear Playlist</button>
       </form>
+      <div>
+        <input
+        value={search}
+          type="text"
+          placeholder="Canciones..."
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button onClick={HandleSearch}>🔍︎</button>
+        <button onClick={handleDeleteSearch}>x</button>
+      </div>
+      <div>
+        {NewSongName ? (
+          NewSongName.map((song, index) => (
+            <div key={index} className="mb-4">
+              <h2 className="text-lg font-semibold">{song.name}</h2>
+              <img src={song.image} alt={song.name} className="max-w-xs" />
+              <button onClick={() => handleSelect(song.name)}>+</button>
+            </div>
+          ))
+        ) : (
+          <p>Busca tus canciones</p>
+        )}
+      </div>
+      <div>
+        <h1>Canciones Seleccionadas</h1>
+      {playlist.songs.length > 0 ? (
+  playlist.songs.map((song, index) => (
+    <div key={index} className="mb-4">
+      <h2 className="text-lg font-semibold">{song}</h2>
+      <button onClick={() => handleDeleteSong(index)}>x</button>
+    </div>
+  ))
+) : (
+  <p>No se han agregado canciones aún.</p>
+)}</div>
+
+      <div>
+        <button type="submit">CREAR PLAYLIST</button>
+      </div>
     </div>
   );
 };

3ad366114ce9bdaaab5c7de046733901925b3e30
Valentina Ortiz <svalentinaog10@gmail.com>
2023-09-20 00:01:37 -0500

    Changes generals

--- src/Pages/ContactPage/Contact.jsx
+++ src/Pages/ContactPage/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import BaseLayout from "../../Components/BaseLayout";
+import "../../Styles/Contact.css";
 
 export default function FormContact() {
   const [formData, setFormData] = useState({
@@ -26,69 +27,77 @@ export default function FormContact() {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-between">
       <BaseLayout>
-        <div className="flex-grow my-40"> 
+        <div className="flex-grow my-40">
           <div className="flex items-center justify-center">
-            <div className="bg-[#1c2026] p-4 sm:p-8 rounded-md shadow-md flex flex-col sm:flex-row w-full max-w-screen-xl">
+            <div className="bg-[#90909037] p-4 sm:p-8 rounded-md shadow-md flex flex-col sm:flex-row w-full max-w-screen-xl">
               <div className="w-full sm:w-1/2 sm:pr-4">
-                <h1 className="text-3xl text-[#54e360] font-bold mb-4">Contáctanos</h1>
+                <h1 className="text-3xl text-[#54e360] font-bold mb-4">
+                  Contáctanos
+                </h1>
                 <p className="text-white mb-4">
                   Bienvenido a nuestra página de contacto.
-                  <br/>
-                  <br/> Estamos encantados de poder conectarnos contigo y ayudarte en lo que necesites. Tu opinión es muy importante para nosotros, así que no dudes en ponerte en contacto para hacernos saber tus preguntas, comentarios o sugerencias.
+                  <br />
+                  <br /> Estamos encantados de poder conectarnos contigo y
+                  ayudarte en lo que necesites. Tu opinión es muy importante
+                  para nosotros, así que no dudes en ponerte en contacto para
+                  hacernos saber tus preguntas, comentarios o sugerencias.
                 </p>
                 <form onSubmit={handleSubmit}>
                   <div className="mb-4">
                     <input
-                      className="w-full p-2 border rounded-md"
+                      className="contact-input"
                       type="text"
                       name="name"
-                      placeholder="Nombre"
+                      placeholder="Tu nombre"
                       value={formData.name}
                       onChange={handleChange}
                     />
                   </div>
                   <div className="mb-4">
                     <input
-                      className="w-full p-2 border rounded-md"
+                      className="contact-input"
                       type="email"
                       name="email"
-                      placeholder="Correo"
+                      placeholder="Tu e-mail"
                       value={formData.email}
                       onChange={handleChange}
                     />
                   </div>
                   <div className="mb-4">
                     <textarea
-                      className="w-full p-2 border rounded-md"
+                      className="text-white w-full p-2 mt-3 mb-1 border-green-500 border rounded-md bg-[#00000047] focus:outline-none"
                       name="message"
-                      placeholder="Mensaje"
+                      placeholder="Cuéntanos cómo podemos ayudarte"
                       value={formData.message}
                       onChange={handleChange}
                     />
                   </div>
-                  <button className="bg-[#54e360] text-white p-2 rounded-md hover:bg-[#00b44b] w-full" type="submit">
-                    Enviar Mensaje
+                  <button
+                    className="bg-[#1ed760] font-bold text-black p-2 rounded-md hover:bg-[#18b54f] w-full"
+                    type="submit"
+                  >
+                    Enviar
                   </button>
                 </form>
               </div>
               <div className="w-full mt-9 sm:w-1/2 sm:pl-4 relative">
                 <div className="relative">
                   <img
-                    src="https://hiramnoriega.com/wp-content/uploads/2023/02/Copia-de-General-2023-02-09T111624.350.jpg"
+                    src="/images/musica-linea-computadora-portatil.jpg"
                     alt="ImagenForm"
                     className="w-full h-auto rounded-lg overflow-hidden shadow-md"
                   />
-                  <div className="absolute bg-[#9b9b9b] p-4 w-full max-w-md mx-auto -mt-16 rounded-md shadow-md">
-                    <div className="flex items-center">
-                      <img src="https://static.vecteezy.com/system/resources/thumbnails/006/827/459/small/email-icon-sign-symbol-logo-vector.jpg" alt="Email" className="w-8 h-8 mr-2" />
-                      <p className="text-gray-700">
-                        Correo electrónico: <a href="mailto:tu@email.com">spootchat@hotmail.com</a>
+                  <div className="absolute bg-[#FFFFFF] p-4 w-full max-w-md mx-auto -mt-16 rounded-md shadow-md">
+                    <div className="flex items-center cursor-default">
+                      <i className="material-icons icon-margin">email</i>
+                      <p className="text-black">
+                        <a href="mailto:tu@email.com">spootchat@hotmail.com</a>
                       </p>
                     </div>
-                    <div className="flex items-center mt-2">
-                      <img src="https://tokystorage.s3.amazonaws.com/company-pic/fad7c0fc408c27d78dff8f9ed83d2b7f-cover.png?v=1564178405" alt="Telefono" className="w-8 h-8 mr-2" />
-                      <p className="text-gray-700">
-                        Número de teléfono: <a href="tel:+123456789">+123 456 789</a>
+                    <div className="flex items-center mt-2 cursor-default">
+                      <i className="material-icons icon-margin">phone</i>
+                      <p className="text-black">
+                        <a href="tel:+123456789">+123 456 789</a>
                       </p>
                     </div>
                   </div>
